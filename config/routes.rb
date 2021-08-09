88fa549c23a6a64f29e41cb3f0bfc492c2cad586
LGmedia954 <LeslieGonzalez@me.com>
2021-08-08 20:01:21 -0400

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,21 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
-  # Some of the following could be refactored.
+  # Some of the following may be refactored.
   # I am writing them out full for now.
 
   #root 'welcome#home'
 
   get 'about', to: 'static#about'
 
-  #get "/ferrets" => 'ferrets#index', as => :ferrets
-  resources :ferrets, only: [:index, :new, :create, :show]
+  resources :ferrets, only: [:index, :new, :create]
+  get '/ferrets/:id', to: 'ferrets#show', as: 'ferret'
 
   #get "/owners" => 'owners#index', as => :owners
   resources :owners, only: [:index, :show]
 
-  #get "/opportunities" => 'opportunities#index', as => :opportunities
-  resources :opportunities, only: [:index, :new, :create, :show]
+  resources :opportunities, only: [:index, :new, :create]
+  get '/opportunities/:id', to: 'opportunities#show', as: 'opportunity'
 
 
 
