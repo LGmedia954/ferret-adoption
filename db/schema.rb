ff4c8842c898674388ed245679c8f3d6342b6413
LGmedia954 <LeslieGonzalez@me.com>
2021-08-17 10:32:15 -0400

    more troubleshooting of PW issue

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_15_132154) do
+ActiveRecord::Schema.define(version: 2021_08_17_121212) do
 
   create_table "ferrets", force: :cascade do |t|
     t.string "name"
