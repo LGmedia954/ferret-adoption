13ec1a4aeb7898339de38c6150d37d047f656074
LGmedia954 <LeslieGonzalez@me.com>
2021-08-20 16:20:38 -0400

    link_to in menu

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -1,4 +1,6 @@
 class OwnersController < ApplicationController
+    before_action :require_logged_in
+    skip_before_action :require_logged_in, only: [:new, :create]
   
     def index
       @owners = Owner.all
