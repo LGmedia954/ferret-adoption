666cf3c95af766410d8d1090b20394b4ca86149f
LGmedia954 <LeslieGonzalez@me.com>
2021-09-03 11:53:48 -0400

    adopter oassociation on Owner model did not work

--- app/models/opportunity.rb
+++ app/models/opportunity.rb
@@ -1,7 +1,6 @@
 class Opportunity < ApplicationRecord
     belongs_to :ferret
     belongs_to :owner
-    belongs_to :adopter, class_name: :owner, foreign_key: "adopter_id"
 
     validates :title, presence: true
     validates :circumstance, length: { maximum: 500 }
